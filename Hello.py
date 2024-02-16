14e2fa3c665002035aa1b0d666c456b18726bff7
Jerrie Kraus-Liang <jerriekrausliang@gmail.com>
2024-02-16 12:56:39 -0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -13,39 +13,29 @@
 # limitations under the License.
 
 import streamlit as st
-from streamlit.logger import get_logger
+import subprocess
+import os
 
-LOGGER = get_logger(__name__)
+# Function to run cookiecutter
+def run_cookiecutter(template_path, extra_args):
+    command = ["cookiecutter", template_path] + extra_args
+    subprocess.run(command, check=True)
 
+# Streamlit UI
+st.title("Project Initialization")
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Collect inputs
+project_name = st.text_input("Project Name")
+other_parameter = st.text_input("Other Parameter")
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
+if st.button("Create Project"):
+    if project_name:
+        # Assuming you want to pass these as arguments to cookiecutter
+        args = ['--no-input', f'project_name={project_name}', f'other_parameter={other_parameter}']
+        run_cookiecutter('/path/to/cookiecutter-template', args)
+        st.success("Project Created")
+    else:
+        st.error("Please enter a project name")
 
 if __name__ == "__main__":
     run()
